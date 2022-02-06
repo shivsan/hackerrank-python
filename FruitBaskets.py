7982a9c3b0ded59ac94c8650bdb21a9a7befde95
Siva Kumar <shivku.goku@gmail.com>
2022-02-06 18:32:44 +0530

    Fruits basket - Optimised

--- FruitBaskets.py
+++ FruitBaskets.py
@@ -22,3 +22,33 @@ def getMaxFruits(fruitArray):
         maxFruits = max(maxFruits, fruits)
 
     return maxFruits
+
+
+def getMaxFruits_optimised(fruitArray):
+    s = 0
+    m = 0
+    b1 = fruitArray[s]
+    b2 = -1
+    maxFruits = 1
+
+    for i in range(1, len(fruitArray)):
+        if fruitArray[i] == b1:
+            # maxFruits = max(maxFruits, i - s + 1)
+            if fruitArray[m] == b2:
+                m = i
+        elif fruitArray[i] == b2:
+            # maxFruits = max(maxFruits, i - s + 1)
+            if fruitArray[m] == b1:
+                m = i
+        elif b2 == -1:
+            b2 = fruitArray[i]
+            # maxFruits = max(maxFruits, i - s + 1)
+            m = i
+        elif fruitArray[i] != b2 and fruitArray[i] != b1:
+            maxFruits = max(maxFruits, (i - 1) - s + 1)
+            s = m
+            m = i
+            b1 = fruitArray[s]
+            b2 = fruitArray[i]
+            maxFruits = max(maxFruits, i - s + 1)
+    return max(maxFruits, len(fruitArray) - s)
--- test_FruitBaskets.py
+++ test_FruitBaskets.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from FruitBaskets import getMaxFruits
+from FruitBaskets import getMaxFruits, getMaxFruits_optimised
 
 
 class Test(TestCase):
@@ -8,3 +8,10 @@ class Test(TestCase):
         self.assertEquals(getMaxFruits([1, 2, 3, 2, 2]), 4)
         self.assertEquals(getMaxFruits([1, 2, 1]), 3)
         self.assertEquals(getMaxFruits([0, 1, 2, 2]), 3)
+
+    def test_get_max_fruits_optimised(self):
+        self.assertEquals(getMaxFruits_optimised([0, 0, 1, 1]), 4)
+        self.assertEquals(getMaxFruits_optimised([1, 2, 3, 2, 2]), 4)
+        self.assertEquals(getMaxFruits_optimised([1, 2, 1]), 3)
+        self.assertEquals(getMaxFruits_optimised([0, 1, 2, 2]), 3)
+        self.assertEquals(getMaxFruits_optimised([0, 1, 2]), 2)
