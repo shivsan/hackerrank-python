ab28e684bf12143e78736cf4997fea0807d36fea
Siva Kumar <shivku.goku@gmail.com>
2022-02-06 23:34:32 +0530

    Max earnings taxi - optimised - almost

--- MaxEarningsFromTaxi.py
+++ MaxEarningsFromTaxi.py
@@ -10,22 +10,49 @@ def getMaxEarningsFromTaxiRec(n: int,
                               currentN: int,
                               rides: List[List[int]],
                               currentRideIndex: int,
-                              maxSoFar: int
+                              memo: dict
                               ):
-    # Stop if all rides are selected and count the money
+    # Stop if all rides are selected
     if currentRideIndex >= len(rides):
-        return maxSoFar
+        memo[currentN] = 0
+        return 0
+
+    if currentN in memo:
+        return memo[currentN]
 
     # Skip if the ride is behind the current position of the passenger. Although, this can't happen if skipped in currentN
     if currentN > rides[currentRideIndex][0]:
-        return getMaxEarningsFromTaxiRec(n, currentN, rides, currentRideIndex + 1, maxSoFar)
+        rec = getMaxEarningsFromTaxiRec(n, currentN, rides, currentRideIndex + 1, memo)
+        memo[currentN] = max(memo[currentN], rec)
+        return rec
 
     # Pick or skip the ride
-    return max(getMaxEarningsFromTaxiRec(n, rides[currentRideIndex][1], rides, currentRideIndex + 1, maxSoFar + getMoney(rides[currentRideIndex])),  # pick
-               getMaxEarningsFromTaxiRec(n, currentN, rides, currentRideIndex + 1, maxSoFar))  # skip
+    result = max(getMaxEarningsFromTaxiRec(n, rides[currentRideIndex][1], rides, currentRideIndex + 1, memo) + getMoney(
+        rides[currentRideIndex]),  # pick
+                 getMaxEarningsFromTaxiRec(n, currentN, rides, currentRideIndex + 1, memo))  # skip
+
+    memo[currentN] = result
+    return result
 
 
 def getMaxEarningsFromTaxi(n: int, rides: List[List[int]]):
     rides.sort(key=lambda ride: ride[0])
+    memo = {}
 
-    return getMaxEarningsFromTaxiRec(n, 0, rides, 0, 0)
+    getMaxEarningsFromTaxiRec(n, 0, rides, 0, memo)
+    return memo[0]
+
+
+# Optimised - bottom up
+
+def getMaxEarningsFromTaxiOptimisedRec(n: int,
+                                       currentN: int,
+                                       rides: List[List[int]],
+                                       currentRideIndex: int):
+    return 0
+
+
+def getMaxEarningsFromTaxiOptimised(n: int, rides: List[List[int]]):
+    rides.sort(key=lambda ride: ride[0])
+
+    return getMaxEarningsFromTaxiOptimisedRec(n, 0, rides, 0)
